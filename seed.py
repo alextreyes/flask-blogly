30f3ba1ad5746353d9fb5eaa2638b5c856d8b2b6
alextreyes <alextreyes11@gmail.com>
2023-12-12 13:05:56 -0500

    requirements

--- seed.py
+++ seed.py
@@ -1,21 +1,21 @@
 from datetime import datetime
 from app import app
-from models import db, User, Post  # Import the User and Post models
+from models import db, User, Post  
 
 db.drop_all()
 db.create_all()
 
 new_user = User(first_name='alex', last_name='reyes')
-# Create a new post
+
 new_post = Post(
     title="New Post Title",
     content="This is the content of the new post.",
-    created_at=datetime.utcnow(),  # You can customize the creation date if needed
-    user_id=1  # Set the user_id foreign key for the post
+    created_at=datetime.utcnow(), 
+    user_id=1  
 )
 
 
-# Commit the changes to the database
+
 db.session.add(new_user)
 db.session.add(new_post)
 db.session.commit()
